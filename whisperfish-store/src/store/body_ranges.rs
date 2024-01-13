7e5721de9dcb7656574ac1e3e2fc5492419ca770
Ruben De Smet <ruben.de.smet@rubdos.be>
2024-01-13 13:36:34 +0100

    Implement wire -> db and db -> wire

--- whisperfish-store/src/store/body_ranges.rs
+++ whisperfish-store/src/store/body_ranges.rs
@@ -1,12 +1,33 @@
 use crate::store::protos as database_protos;
-use libsignal_service::proto::BodyRange as WireBodyRange;
+use libsignal_service::proto::{body_range as wire_body_range, BodyRange as WireBodyRange};
+use prost::Message;
 
 pub fn serialize(value: &[WireBodyRange]) -> Option<Vec<u8>> {
     if value.is_empty() {
         return None;
     }
 
-    todo!()
+    let message_ranges = database_protos::BodyRangeList {
+        ranges: value
+            .iter()
+            .map(|range| database_protos::body_range_list::BodyRange {
+                start: range.start.expect("start") as i32,
+                length: range.length.expect("end") as i32,
+                associated_value: range.associated_value.as_ref().map(|av| match av {
+                    wire_body_range::AssociatedValue::MentionAci(mention_aci) => {
+                        database_protos::body_range_list::body_range::AssociatedValue::MentionUuid(
+                            mention_aci.clone(),
+                        )
+                    }
+                    wire_body_range::AssociatedValue::Style(style) => {
+                        database_protos::body_range_list::body_range::AssociatedValue::Style(*style)
+                    }
+                }),
+            })
+            .collect(),
+    };
+
+    Some(message_ranges.encode_to_vec())
 }
 
 pub fn to_vec(message_ranges: Option<&Vec<u8>>) -> Vec<WireBodyRange> {
@@ -14,5 +35,38 @@ pub fn to_vec(message_ranges: Option<&Vec<u8>>) -> Vec<WireBodyRange> {
         return vec![];
     };
 
-    todo!()
+    let message_ranges = database_protos::BodyRangeList::decode(message_ranges as &[u8])
+        .expect("valid protobuf in database");
+    message_ranges
+        .ranges
+        .iter()
+        .flat_map(|range| {
+            let associated_value = match range
+                .associated_value
+                .as_ref()
+                .expect("associated_value in db")
+            {
+                database_protos::body_range_list::body_range::AssociatedValue::MentionUuid(
+                    mention_aci,
+                ) => wire_body_range::AssociatedValue::MentionAci(mention_aci.clone()),
+                database_protos::body_range_list::body_range::AssociatedValue::Style(style) => {
+                    wire_body_range::AssociatedValue::Style(*style)
+                }
+                database_protos::body_range_list::body_range::AssociatedValue::Link(link) => {
+                    tracing::warn!("Not encoding link {link}");
+                    return None;
+                }
+                database_protos::body_range_list::body_range::AssociatedValue::Button(button) => {
+                    tracing::warn!("Not encoding button {button:?}");
+                    return None;
+                }
+            };
+
+            Some(WireBodyRange {
+                start: Some(range.start as u32),
+                length: Some(range.length as u32),
+                associated_value: Some(associated_value),
+            })
+        })
+        .collect()
 }
