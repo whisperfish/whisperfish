4f974dcf4b847273adf4b8b9102616f7c39a0c54
Ruben De Smet <ruben.de.smet@rubdos.be>
2024-02-08 12:03:44 +0100

    Put both boolings in index entry instead of where clause

--- migrations/2024-02-07-183018_index-pending-messages/up.sql
+++ migrations/2024-02-07-183018_index-pending-messages/up.sql
@@ -1,1 +1,1 @@
-CREATE INDEX failed_messages ON messages(sending_has_failed) WHERE sent_timestamp IS NULL and is_outbound = TRUE;
+CREATE INDEX failed_messages ON messages(sending_has_failed, is_outbound) WHERE sent_timestamp IS NULL;
