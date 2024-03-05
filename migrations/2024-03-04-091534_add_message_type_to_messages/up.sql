34b4b4c33ad066b87ccbc7f2117c8d4df0bbc6a2
Ruben De Smet <ruben.de.smet@rubdos.be>
2024-03-05 13:26:02 +0000

    Use snake_case strings for storing message type

--- migrations/2024-03-04-091534_add_message_type_to_messages/up.sql
+++ migrations/2024-03-04-091534_add_message_type_to_messages/up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE messages
     ADD COLUMN message_type TEXT;
-UPDATE messages SET message_type = "Endsession" WHERE flags = 1;
-UPDATE messages SET message_type = "ExpirationTimerUpdate" WHERE flags = 2;
-UPDATE messages SET message_type = "ProfileKeyUpdate" WHERE flags = 4;
-UPDATE messages SET message_type = "GroupChange" WHERE (text LIKE "Group changed by %" AND LENGTH(text) = 53) OR text = "Group changed by nobody" OR text LIKE "Group changed by +%";
+UPDATE messages SET message_type = "end_session" WHERE flags = 1;
+UPDATE messages SET message_type = "expiration_timer_update" WHERE flags = 2;
+UPDATE messages SET message_type = "profile_key_update" WHERE flags = 4;
+UPDATE messages SET message_type = "group_change" WHERE (text LIKE "Group changed by %" AND LENGTH(text) = 53) OR text = "Group changed by nobody" OR text LIKE "Group changed by +%";
 CREATE INDEX messages_message_type ON messages (message_type);
