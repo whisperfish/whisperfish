dbdeffcf3f89fb73f94a6332d41165e75e8df108
Matti Viljanen <matti.viljanen@kapsi.fi>
2024-03-17 11:29:04 +0200

    Autodetect and use lupdate or lupdate-qt5

--- update-translations.sh
+++ update-translations.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+LUPDATE=$(which lupdate 2>/dev/null || which lupdate-qt5 2>/dev/null)
+if [ ! -f "$LUPDATE" ]; then
+  echo "lupdate or lupdate-qt5 not found in \$PATH"
+  exit 1
+fi
 
-lupdate qml/ -noobsolete -ts translations/*.ts
+$LUPDATE qml/ -noobsolete -ts translations/*.ts
