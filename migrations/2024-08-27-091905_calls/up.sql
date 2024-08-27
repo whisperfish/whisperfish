7cd5543d449ba252b5a7bd5359d37bd9cba7d916
Ruben De Smet <ruben.de.smet@rubdos.be>
2024-08-27 11:34:54 +0200

    Initial calls table

---
+++ migrations/2024-08-27-091905_calls/up.sql
@@ -0,0 +1,24 @@
+CREATE TABLE calls (
+    id INTEGER PRIMARY KEY NOT NULL,
+    call_id INTEGER NOT NULL,
+    message_id INTEGER DEFAULT NULL REFERENCES messages(id) ON DELETE SET NULL,
+    session_id INTEGER NOT NULL REFERENCES sessions(id) ON DELETE CASCADE,
+    type TEXT CHECK(type IN (
+            'audio',
+            'video',
+            'group',
+            'ad_hoc'
+    )) NOT NULL,
+    is_outbound BOOLEAN NOT NULL,
+    -- More events (enum class Event in CallTable)
+    event TEXT CHECK(type IN ('accepted', 'not_accepted', 'observed')) NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    ringer INTEGER NOT NULL REFERENCES recipients(id) ON DELETE CASCADE,
+    -- DELETEed calls exist in the database for some hours as to prevent out-of-order reappearance of calls
+    deletion_timestamp TIMESTAMP DEFAULT NULL,
+    is_read BOOLEAN NOT NULL,
+    local_joined BOOLEAN NOT NULL DEFAULT FALSE,
+    group_call_active BOOLEAN NOT NULL DEFAULT FALSE,
+
+    UNIQUE (call_id, session_id) ON CONFLICT FAIL
+)
