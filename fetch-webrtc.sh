6defd08a466e584615b52a10bd17b61aad151e78
Ruben De Smet <ruben.de.smet@rubdos.be>
2024-09-08 14:52:34 +0200

    Add x64 webrtc support

--- fetch-webrtc.sh
+++ fetch-webrtc.sh
@@ -6,13 +6,13 @@ BASE_URL="https://nas.rubdos.be/~rsmet/webrtc/"
 declare -A WEBRTC_HASHES
 WEBRTC_HASHES[arm]="56d4809b7d034816185b2f165a56514e29a799a6a5be1528a53c72a42990e275bf6c2895076fce991704f9899acfe280"
 WEBRTC_HASHES[arm64]="28e0605917aa99b34303ee8b59eb0495b2bb3056ca9be2a5a553d34ac21d067324afd0bef06ac91cb266a7ad04dac4ba"
-# WEBRTC_HASHES[x64]=""
+WEBRTC_HASHES[x64]="337860360916a03c0a0da3e44f002f9cf3083c38ad4b4de9a9052a6ff50c9fc909433cabccaf6075554056d29408558f"
 WEBRTC_HASHES[x86]="89143eb3464547263770cffc66bb741e4407366ac4a21e695510fb3474ddef4b5bf30eb5b1abac3060b1d9b562c6cbab"
 
 declare -A ARCHS
 ARCHS[arm]="armv7-unknown-linux-gnueabihf"
 ARCHS[arm64]="aarch64-unknown-linux-gnu"
-# ARCHS[]="x86_64"
+ARCHS[x64]="x86_64-unknown-linux-gnu"
 ARCHS[x86]="i686-unknown-linux-gnu"
 
 for arch in "${!ARCHS[@]}"; do
--- whisperfish/build.rs
+++ whisperfish/build.rs
@@ -44,7 +44,7 @@ fn configure_webrtc() -> anyhow::Result<()> {
     let files = [
         ("arm", "56d4809b7d034816185b2f165a56514e29a799a6a5be1528a53c72a42990e275bf6c2895076fce991704f9899acfe280"),
         ("arm64", "28e0605917aa99b34303ee8b59eb0495b2bb3056ca9be2a5a553d34ac21d067324afd0bef06ac91cb266a7ad04dac4ba"),
-        // ("x64", ""),
+        ("x64", "337860360916a03c0a0da3e44f002f9cf3083c38ad4b4de9a9052a6ff50c9fc909433cabccaf6075554056d29408558f"),
         ("x86", "89143eb3464547263770cffc66bb741e4407366ac4a21e695510fb3474ddef4b5bf30eb5b1abac3060b1d9b562c6cbab"),
     ].iter().cloned().collect::<std::collections::HashMap<&str, &str>>();
 
@@ -52,7 +52,7 @@ fn configure_webrtc() -> anyhow::Result<()> {
     let archs = [
         ("armv7-unknown-linux-gnueabihf", "arm"),
         ("aarch64-unknown-linux-gnu", "arm64"),
-        // ("x86_64", ""),
+        ("x86_64-unknown-linux-gnu", "x64"),
         ("i686-unknown-linux-gnu", "x86"),
     ]
     .iter()
