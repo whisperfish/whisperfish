0e650009ec4aa06cd0f25d8e07b28d0e220c9af4
Matti Viljanen <matti.viljanen@kapsi.fi>
2024-10-03 20:39:10 +0300

    Clean up master key startup migration
    Co-authored-by: Ruben De Smet <ruben.de.smet@rubdos.be>

--- whisperfish/src/worker/client/migrations/master_key.rs
+++ whisperfish/src/worker/client/migrations/master_key.rs
@@ -16,73 +16,56 @@ impl Handler<CheckMasterKey> for ClientActor {
             .as_ref()
             .expect("storage in check master key migration")
             .clone();
-        let curr_master_key = storage.fetch_master_key();
-        let _config = self.config.as_ref();
+
+        if storage.fetch_master_key().is_some() {
+            tracing::debug!("Whisperfish has master key.");
+            return Box::pin(future::ready(()).into_actor(self).map(
+                move |_res, act: &mut Self, _ctx| {
+                    act.migration_state.notify_check_master_key();
+                },
+            ));
+        };
+
+        let config = self.config.clone();
         let sender = self.message_sender();
         let is_primary = self.config.as_ref().get_device_id() == DEFAULT_DEVICE_ID.into();
-        let addr = ctx.address();
+        let ctx_addr = ctx.address();
 
         Box::pin(
             async move {
                 connectable.await;
-                let mut success = true;
-                match (is_primary, curr_master_key) {
-                    (true, None) => {
-                        tracing::debug!(
-                            "Whisperfish is primary and doesn't have master key. Generating..."
-                        );
-                        let master_key = MasterKey::generate();
-                        let storage_key = StorageServiceKey::from_master_key(&master_key);
-                        storage.store_master_key(Some(&master_key));
-                        storage.store_storage_service_key(Some(&storage_key));
-                    }
-                    (false, None) => {
-                        tracing::debug!(
-                            "Whisperfish is linked and doesn't have master key. Fetching..."
-                        );
+                if is_primary {
+                    tracing::debug!("Whisperfish is primary. Generating master key...");
+                    let master_key = MasterKey::generate();
+                    let storage_key = StorageServiceKey::from_master_key(&master_key);
+                    storage.store_master_key(Some(&master_key));
+                    storage.store_storage_service_key(Some(&storage_key));
+                    Ok(true)
+                } else {
+                    tracing::debug!("Whisperfish is secondary. Fetching master key...");
 
-                        match sender.await {
-                            Ok(mut sender) => {
-                                let self_recipient = storage
-                                    .fetch_self_recipient()
-                                    .expect("self recipient in initialize MasterKey");
-                                let self_address = self_recipient
-                                    .to_aci_service_address()
-                                    .expect("self ACI ServiceAddress in initialize MasterKey");
-                                sender
-                                    .send_sync_message_request(&self_address, RequestType::Keys)
-                                    .await
-                                    .unwrap();
-                            }
-                            Err(e) => {
-                                tracing::error!(
-                                    "Could not send MasterKey request: {:?} Retrying in 10 seconds.",
-                                    e
-                                );
-                                tokio::time::sleep(Duration::from_secs(10)).await;
-                                success = false;
-                            }
+                    match sender.await {
+                        Ok(mut sender) => {
+                            let addr = ServiceAddress::new_aci(config.get_aci().unwrap());
+                            let req = RequestType::Keys;
+                            sender.send_sync_message_request(&addr, req).await.unwrap();
+                            Ok(true)
+                        }
+                        Err(e) => {
+                            tracing::error!("Try refetching master key in 10 seconds: {e:?}");
+                            tokio::time::sleep(Duration::from_secs(10)).await;
+                            Ok(false)
                         }
                     }
-                    (_, Some(_)) => tracing::debug!("Whisperfish has master key in database"),
-                };
-
-                Ok(success)
+                }
             }
             .instrument(tracing::debug_span!("Initialize MasterKey"))
             .into_actor(self)
             .map(
                 move |result: anyhow::Result<bool>, act: &mut Self, _ctx| match result {
-                    Err(e) => {
-                        tracing::error!("Error initializing MasterKey: {e:#}");
-                    }
-                    Ok(true) => {
-                        act.migration_state.notify_check_master_key();
-                    }
-                    Ok(false) => {
-                        // Delay was awaited where success was set to false
-                        addr.do_send(CheckMasterKey);
-                    }
+                    Err(e) => tracing::error!("Error initializing MasterKey: {e:#}"),
+                    Ok(true) => act.migration_state.notify_check_master_key(),
+                    Ok(false) => ctx_addr.do_send(CheckMasterKey),
                 },
             ),
         )
