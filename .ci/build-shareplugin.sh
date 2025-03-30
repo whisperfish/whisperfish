06305795e70b6924349a4ef21a48374ea3c6f8f4
Ruben De Smet <ruben.de.smet@rubdos.be>
2025-03-30 19:45:53 +0200

    Add .git as safe source dir, add timestamp prints to shareplugin build

--- .ci/build-shareplugin.sh
+++ .ci/build-shareplugin.sh
@@ -2,13 +2,19 @@
 
 set -e
 
-echo adding $PWD as safe directory in git
-git config --global --add safe.directory $PWD
+echo_t() {
+    echo "[$(date +%H:%M:%S)]" "$@"
+}
 
-echo "Building for $SFOS_VERSION"
+echo_t "Building for $SFOS_VERSION"
+
+echo_t "Adding $PWD as safe directory in git..."
+git config --global --add safe.directory "$PWD"
+
 # The MB2 image comes with a default user.
 # We need to copy the source over, because of that.
 
+echo_t "Cloning Whisperfish..."
 git clone . ~/whisperfish-build
 pushd ~/whisperfish-build
 
@@ -19,6 +25,7 @@ cd "shareplugin_v$SHAREPLUGIN_VERSION"
 # -f to ignore non-existent files
 rm -f RPMS/*.rpm
 
+echo_t "Building the shareplugin..."
 mb2 -t SailfishOS-$SFOS_VERSION-$MER_ARCH --no-snapshot=force build \
     --enable-debug \
     --no-check
@@ -27,7 +34,10 @@ mb2 -t SailfishOS-$SFOS_VERSION-$MER_ARCH --no-snapshot=force build \
 
 # Copy everything useful back
 popd
+echo_t "Copying RPM files..."
 mkdir -p RPMS target
 sudo cp -ar ~/whisperfish-build/shareplugin_v$SHAREPLUGIN_VERSION/RPMS/* RPMS/
 
+echo_t "Uploading RPM files to GitLab..."
 .ci/upload-rpms.sh
+echo_t "Done!"
