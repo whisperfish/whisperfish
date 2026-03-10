c895c57b9c994c8626d46ff69008c4b23a1269c7
Ruben De Smet <weblate@rubdos.be>
2026-03-10 14:01:39 +0100

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (437 of 437 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/nl/

--- translations/harbour-whisperfish-nl.ts
+++ translations/harbour-whisperfish-nl.ts
@@ -602,7 +602,7 @@
         <source>Loading messages</source>
         <oldsource>Synchronizing</oldsource>
         <extracomment>Whisperfish connection status message</extracomment>
-        <translation type="unfinished">Synchroniseren</translation>
+        <translation>Berichten laden</translation>
     </message>
     <message id="whisperfish-registration-required-message">
         <location filename="../qml/pages/MainPage.qml" line="297"/>
